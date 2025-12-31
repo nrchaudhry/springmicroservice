43b1a78cebf5d682d6238203eb67f11f69e21d4e
Nauman Riaz Chaudhry <nauman.riaz@gmail.com>
2025-12-31 10:57:07 +0500

    Add isLive flag to control service registration

--- src/main/java/com/cwiztech/config/EndpointsListener.java
+++ src/main/java/com/cwiztech/config/EndpointsListener.java
@@ -1,4 +1,4 @@
-package com.cwiztech.config;
+  package com.cwiztech.config;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -23,52 +23,56 @@ import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandl
 public class EndpointsListener implements ApplicationListener<ContextRefreshedEvent> {
 	private static String servicePath;
 	private static String oauthapplicationPath;
+	private static boolean isLive;
 
 	public EndpointsListener(Environment env) {
 		EndpointsListener.servicePath = env.getRequiredProperty("file_path.servicePath");
 		EndpointsListener.oauthapplicationPath = env.getRequiredProperty("file_path.oauthapplicationPath");
+		EndpointsListener.isLive = Boolean.parseBoolean(env.getRequiredProperty("file_path.isLive"));
 	}
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
-    	List<String> endpoints = new ArrayList<String>();
-    	JSONArray applicationServices = new JSONArray();
-    	Socket socket = new Socket();
-    	try {
-			socket.connect(new InetSocketAddress("google.com", 80));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	
-        ApplicationContext applicationContext = event.getApplicationContext();
-        applicationContext.getBean(RequestMappingHandlerMapping.class).getHandlerMethods()
-             .forEach((key, value) -> endpoints.add(key.toString()));
-        
-        for (int i=0; i<endpoints.size(); i++) {
-        	JSONObject applicationService = new JSONObject();
-        	String services[] = endpoints.get(i).substring(1, endpoints.get(i).length()-1).split(",");
-        	
-        	if (endpoints.get(i).contains("[/error]") == false) {
-	        	applicationService.put("microservice_URI", EndpointsListener.servicePath+services[0].replace("[", "").replace("]", ""));
-	        	applicationService.put("microservice_PATH", "http:/"+socket.getLocalAddress()+":8080/");
-	        	applicationService.put("microservice_METHOD", services[1].replace("methods=", "").replace("[", "").replace("]", ""));
-	        	if (endpoints.get(i).contains("[GET]") && services[0].replace("[", "").replace("]", "").lastIndexOf("/") == 0)
-		        	applicationService.put("ismainpath", "Y");
-	        	else
-		        	applicationService.put("ismainpath", "N");
-	        	applicationServices.put(applicationService);
-        	}
-        }
-        
-		RestTemplate restTemplate = new RestTemplate();
-		HttpHeaders headers = new HttpHeaders();
-		headers.add("Content-type", "application/json");
-		headers.add("Authorization", "serviceregistry");
-		headers.add("Grant_Type", "password");
-		String appPath = EndpointsListener.oauthapplicationPath;
-
-		HttpEntity<String> entity = new HttpEntity<String>(applicationServices.toString().replace("\"", "'"), headers);
-		restTemplate.exchange(appPath + "microservice", HttpMethod.PUT, entity, String.class);
+    	if (isLive == true) {
+	    	List<String> endpoints = new ArrayList<String>();
+	    	JSONArray applicationServices = new JSONArray();
+	    	Socket socket = new Socket();
+	    	try {
+				socket.connect(new InetSocketAddress("google.com", 80));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	    	
+	        ApplicationContext applicationContext = event.getApplicationContext();
+	        applicationContext.getBean(RequestMappingHandlerMapping.class).getHandlerMethods()
+	             .forEach((key, value) -> endpoints.add(key.toString()));
+	        
+	        for (int i=0; i<endpoints.size(); i++) {
+	        	JSONObject applicationService = new JSONObject();
+	        	String services[] = endpoints.get(i).substring(1, endpoints.get(i).length()-1).split(",");
+	        	
+	        	if (endpoints.get(i).contains("[/error]") == false) {
+		        	applicationService.put("microservice_URI", EndpointsListener.servicePath+services[0].replace("[", "").replace("]", ""));
+		        	applicationService.put("microservice_PATH", "http:/"+socket.getLocalAddress()+":8080/");
+		        	applicationService.put("microservice_METHOD", services[1].replace("methods=", "").replace("[", "").replace("]", ""));
+		        	if (endpoints.get(i).contains("[GET]") && services[0].replace("[", "").replace("]", "").lastIndexOf("/") == 0)
+			        	applicationService.put("ismainpath", "Y");
+		        	else
+			        	applicationService.put("ismainpath", "N");
+		        	applicationServices.put(applicationService);
+	        	}
+	        }
+	        
+			RestTemplate restTemplate = new RestTemplate();
+			HttpHeaders headers = new HttpHeaders();
+			headers.add("Content-type", "application/json");
+			headers.add("Authorization", "serviceregistry");
+			headers.add("Grant_Type", "password");
+			String appPath = EndpointsListener.oauthapplicationPath;
+	
+			HttpEntity<String> entity = new HttpEntity<String>(applicationServices.toString().replace("\"", "'"), headers);
+			restTemplate.exchange(appPath + "microservice", HttpMethod.PUT, entity, String.class);
+    	}
     }
-}
+} 
